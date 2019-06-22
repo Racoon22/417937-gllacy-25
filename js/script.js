3ee0368aed66b60675ba78d5c25af7a1116dfe0d
alex-antonov <antonovsasha22@mail.ru>
2019-06-22 14:30:44 +0700

    Refactored all edits and minify scripts

--- js/script.js
+++ js/script.js
@@ -1,21 +1,21 @@
-var modalOverlay = document.querySelector(".feedback-overlay");
-var modalOpenButton =  document.querySelector(".btn-contact");
-var modalCloseButton =  document.querySelector(".modal-close");
+var modalOverlay = document.querySelector('.feedback-overlay');
+var modalOpenButton =  document.querySelector('.btn-contact');
+var modalCloseButton =  document.querySelector('.modal-close');
 
 
 modalOpenButton.addEventListener('click', function (e) {
     e.preventDefault();
-    modalOverlay.classList.add("feedback-overlay--show");
+    modalOverlay.classList.add('feedback-overlay--show');
 });
 
 modalCloseButton.addEventListener('click', function (e) {
     e.preventDefault();
-    modalOverlay.classList.remove("feedback-overlay--show");
+    modalOverlay.classList.remove('feedback-overlay--show');
 });
 
-window.addEventListener("keydown", function (e) {
-    if (e.keyCode === 27 && modalOverlay.classList.contains("feedback-overlay--show")) {
+window.addEventListener('keydown', function (e) {
+    if (e.keyCode === 27 && modalOverlay.classList.contains('feedback-overlay--show')) {
         e.preventDefault();
-        modalOverlay.classList.remove("feedback-overlay--show")
+        modalOverlay.classList.remove('feedback-overlay--show')
     }
 });
